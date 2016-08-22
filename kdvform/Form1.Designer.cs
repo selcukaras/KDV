cba60d2d04d52fb5aff24d9c028d3df0838d78f8
selcukaras <selcukalan.net@gmail.com>
2016-08-22 14:13:30 +0300

    KDV

---
+++ kdvform/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace kdvform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbUrunler = new System.Windows.Forms.Label();
+            this.cmb_urun = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.lblFiyat = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbUrunler
+            // 
+            this.cmbUrunler.AutoSize = true;
+            this.cmbUrunler.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbUrunler.Location = new System.Drawing.Point(36, 52);
+            this.cmbUrunler.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.cmbUrunler.Name = "cmbUrunler";
+            this.cmbUrunler.Size = new System.Drawing.Size(78, 20);
+            this.cmbUrunler.TabIndex = 0;
+            this.cmbUrunler.Text = "Ürünler:";
+            // 
+            // cmb_urun
+            // 
+            this.cmb_urun.FormattingEnabled = true;
+            this.cmb_urun.Location = new System.Drawing.Point(148, 46);
+            this.cmb_urun.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmb_urun.Name = "cmb_urun";
+            this.cmb_urun.Size = new System.Drawing.Size(204, 24);
+            this.cmb_urun.TabIndex = 1;
+            this.cmb_urun.SelectedIndexChanged += new System.EventHandler(this.cmb_urun_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(43, 111);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(311, 79);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "KDV";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(227, 38);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(57, 21);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "%20";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(144, 38);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(57, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "%18";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(76, 38);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "%15";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(8, 38);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "%10";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // lblFiyat
+            // 
+            this.lblFiyat.AutoSize = true;
+            this.lblFiyat.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFiyat.Location = new System.Drawing.Point(39, 234);
+            this.lblFiyat.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFiyat.Name = "lblFiyat";
+            this.lblFiyat.Size = new System.Drawing.Size(96, 19);
+            this.lblFiyat.TabIndex = 3;
+            this.lblFiyat.Text = "Satış Fiyatı:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 322);
+            this.Controls.Add(this.lblFiyat);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmb_urun);
+            this.Controls.Add(this.cmbUrunler);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "KDV ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cmbUrunler;
+        private System.Windows.Forms.ComboBox cmb_urun;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label lblFiyat;
+    }
+}
+
---
+++ kdvform/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kdvform
+{
+    public partial class Form1 : Form
+    {
+        private double[] kdvler;
+        private double[] fiyatlar;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmb_urun.Items.Add("Klavye");
+            cmb_urun.Items.Add("Mouse");
+            cmb_urun.Items.Add("Monitor");
+            cmb_urun.Items.Add("Hoparlör");
+            cmb_urun.Items.Add("Yazıcı");
+            cmb_urun.Items.Add("Tarayıcı");
+            kdvler = new double[6];
+            kdvler[0] = 0.1;
+            kdvler[1] = 0.15;
+            kdvler[2] = 0.18;
+            kdvler[3] = 0.20;
+            kdvler[4] = 0.15;
+            kdvler[5] = 0.15;
+            fiyatlar = new double[6];
+            fiyatlar[0] = 25;
+            fiyatlar[1] = 15;
+            fiyatlar[2] = 250;
+            fiyatlar[3] = 35;
+            fiyatlar[4] = 375;
+            fiyatlar[5] = 325;
+        }
+    
+
+
+
+        private void cmb_urun_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int cmbIndex = cmb_urun.SelectedIndex;
+            double kdv = kdvler[cmbIndex];
+            double fiyat = fiyatlar[cmbIndex];
+            double satis = fiyat + fiyat * kdv;
+            string str = String.Format("Satış Fiyatı: {0:0.00}", satis);
+            lblFiyat.Text = str;
+            if (kdv == 0.1)
+            {
+                radioButton1.Select();
+            }
+            else if (kdv == 0.15)
+            {
+                radioButton2.Select();
+            }
+            else if (kdv == 0.18)
+            {
+                radioButton3.Select();
+            }
+            else if (kdv == 0.20)
+            {
+                radioButton4.Select();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            radioSelect();
+        }
+
+        private void radioSelect()
+        {
+            int cmbIndex = cmb_urun.SelectedIndex;
+            double kdv = kdvler[cmbIndex];
+            if (kdv == 0.1)
+            {
+                radioButton1.Select();
+            }
+            else if (kdv == 0.15)
+            {
+                radioButton2.Select();
+            }
+            else if (kdv == 0.18)
+            {
+                radioButton3.Select();
+            }
+            else if (kdv == 0.20)
+            {
+                radioButton4.Select();
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioSelect();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            radioSelect();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            radioSelect();
+        }
+    }
+}
+        
